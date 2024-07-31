e60dc6bc306847a02b5147b20d94cb0e4202bd10
Heliton222 <101953291+Heliton222@users.noreply.github.com>
2024-07-30 21:29:35 -0300

    Add files via upload

---
+++ aula 15/js/script.js
@@ -0,0 +1,37 @@
+const texto = document.getElementById("texto")
+const no = document.getElementById("no")
+const fundo = document.getElementById("fundo")
+
+var contador = 0
+
+no.addEventListener("mousemove",function(){
+    contador++
+    let x = Math.floor( Math.random() *100 )
+    let y = Math.floor( Math.random() *100 )
+
+no.style.bottom = y + "%"
+no.style.right = x + "%"
+
+if( contador == 5 ) {  
+    texto.innerHTML = "pensa bem!"
+
+}
+
+if( contador == 10 ) {  
+    texto.innerHTML = "se eu fosse você pensaria bem!"
+    fundo.style.backgroundImage ='https://acdn.mitiendanube.com/stores/001/134/310/products/revolver-taurus-rt410-cal36-165mm-inox-5tiros_1_12001-337276a94a8dcdfb2116185165375051-1024-1024.webp'
+
+  }
+
+})
+
+function casar() {
+    no.innerHTML = claro
+
+    setTimeout( function(){
+        while( !confirm("confirma o casório?") ){}
+        alert("😍vos declaro casados!!❤")
+        window.close()
+    },500 )
+}
+
